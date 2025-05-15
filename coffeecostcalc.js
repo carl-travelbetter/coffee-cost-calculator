a785033e70b15797760ab0f169b8678381b942a0
carl-travelbetter <cmciver@travelbetter.co.uk>
2025-05-15 21:59:54 +0100

    Update coffeecostcalc.js

--- coffeecostcalc.js
+++ coffeecostcalc.js
@@ -1,4 +1,14 @@
 function calculateCoffeeCost()
 {
-  console.log("Caldulating Coffee Habit Cost");
+  console.log("Calculating Coffee Habit Cost");
+  let coffees = document.getElementById("coffees").value;
+  console.log("Coffees "+coffees);
+  let days = document.getElementById("days").value;
+  console.log("days "+days);
+  let price = document.getElementById("price").value;
+  let coffeeSpend = (price * days * coffees).toFixed(2);
+  console.log("Coffee Spend "+coffeeSpend);
+  let result = `You Spend £${coffeeSpend} on coffee per week`;
+  document.getElementById("result").innerHTML = result;
+    //savingEstimates(coffeeSpend);
 }
